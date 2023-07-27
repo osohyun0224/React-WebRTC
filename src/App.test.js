f65a5b577892933d7dcb32ca1867ffbb1235d907
osohyun0224 <osohyun0224@naver.com>
2023-07-27 20:07:23 +0900

    Fix: React 18 콘솔 오류 수정

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,9 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = ReactDOM.createRoot(div);
+  
+  root.render(<App />);
+
+  root.unmount();
 });
